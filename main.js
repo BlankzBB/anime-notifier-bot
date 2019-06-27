88b4af94aad687676a51604d545151ca1ecb0f44
OneDex <1onedex1@gmail.com>
2019-06-27 09:30:51 -0400

    Added support for manga in search Made search command more in depth specify -t anime/manga to filter to a specific type of media specify -n x to specify how many results you want Can't wait to refactor this or get bleach for my eyes after seeing how i've written it

--- main.js
+++ main.js
@@ -1,75 +1,131 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-console */
 const Eris = require('eris');
 const fetch = require('node-fetch');
-const login = require('./config/login.json')
+const login = require('./config/login.json');
 
+const apiURL = 'https://graphql.anilist.co';
 const client = new Eris.CommandClient(login.discord, {}, {
-    defaultHelpCommand: false,
-    description: 'A notifier bot',
-    owner: 'OneDex',
-    prefix: '!',
-  });
+  defaultHelpCommand: false,
+  description: 'A notifier bot',
+  owner: 'OneDex',
+  prefix: '!',
+});
 
-  client.connect();
+client.connect();
 
 client.registerCommand('search', (message, args) => {
-    const aniSearch = args.join(' ');
-    
-    let query = `query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {
-        Page (page: $page, perPage: $perPage) {
-          media (id: $id, search: $search, type: $type) {
-            id
-            idMal
-            title {
-              romaji,
-              english,
-              native
-            }
-            type
-            status
-            updatedAt
-            startDate {
-              year
-              month
-              day
-            }
-            endDate {
-              year
-              month
-              day
-            }
-            episodes
-            nextAiringEpisode {
-              episode
-              airingAt
-              timeUntilAiring
-            }
-          }
-        }
-      }`
+  const search = [];
 
-    let vars = {
-        search: aniSearch,
-        type: 'ANIME',
-        page: 1,
-        perPage: 3,
+  let vars = {
+    page: 1,
+  };
+
+  let typeLoc = -1;
+  if (args.indexOf('-t') !== -1) {
+    typeLoc = args.indexOf('-t') + 1;
+    vars = Object.assign({ type: args[typeLoc].toUpperCase() }, vars);
+  }
+
+  let pn = 3;
+  let pnLoc = -1;
+  if (args.indexOf('-n') !== -1) {
+    pnLoc = args.indexOf('-n') + 1;
+    if (args[pnLoc] < 10) {
+      pn = Number(args[pnLoc]);
     }
+    if (args[pnLoc] > 10) {
+      pn = 3;
+    }
+  }
+  vars = Object.assign({ perPage: pn }, vars);
+  
+  args.forEach((i, index) => {
+    if (index > typeLoc && index > pnLoc) {
+      search.push(i);
+    }
+  });
+  const txtSearch = search.join(' ');
+  vars = Object.assign({ search: txtSearch }, vars);
 
-    let url = 'https://graphql.anilist.co',
-    options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: vars
-        })
-    };
+  // console.log(vars);
+  const query = `query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {
+    Page (page: $page, perPage: $perPage) {
+      media (id: $id, search: $search, type: $type) {
+        id
+        idMal
+        title {
+          romaji,
+          english,
+          native
+        }
+        type
+        status
+        updatedAt
+        startDate {
+          year
+          month
+          day
+        }
+        endDate {
+          year
+          month
+          day
+        }
+        episodes
+        nextAiringEpisode {
+          episode
+          airingAt
+          timeUntilAiring
+        }
+      }
+    }
+  }`;
 
-    fetch(url, options).then((res) => res.json())
-                   .then((res) => {
-                     console.log(res.data.Page.media[1])
-                     message.channel.createMessage(`title: ${res.data.Page.media[1].title.romaji} (${res.data.Page.media[1].title.english})\nlink: https://myanimelist.net/anime/${res.data.Page.media[1].id}`)
-                   })
+  apiCall(query, vars, (searchList) => {
+    // console.log(searchList);
+    messageCreator(message, searchList);
+  });
+});
+
+function apiCall(query, vars, callback) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: vars,
+    }),
+  };
+  fetch(apiURL, options)
+    .then(res => res.json())
+    .then((res) => {
+      const searchList = res.data.Page.media;
+      callback(searchList);
+    });
+}
+
+function messageCreator(message, searchList) {
+  const msg = [];
+  for (i = 0; i < searchList.length; i++) {
+    let eTitle;
+    if (searchList[i].title.english == null) {
+      eTitle = searchList[i].title.romaji;
+    }
+    if (searchList[i].title.english != null) {
+      eTitle = searchList[i].title.english;
+    }
+    msg.push(`title: ${searchList[i].title.romaji} (${eTitle})\ntype: ${searchList[i].type}\nlink: https://myanimelist.net/${searchList[i].type.toLowerCase()}/${searchList[i].idMal} `);
+  }
+  const res = msg.join('\n\n');
+  // console.log(res);
+  message.channel.createMessage(`${res}`);
+}
+
+client.registerCommand('ping', (message) => {
+  message.channel.createMessage('pong!')
 })
+client.registerCommandAlias('Ping', 'ping');
