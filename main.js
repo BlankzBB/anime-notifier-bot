0f651816c6cc1b15434cb12d4266325d8a534430
OneDex <1onedex1@gmail.com>
2019-07-10 21:33:33 -0400

    Added search back, added console.logs, added reply messages

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-/* TODO
-* Find a way to get next EP without making user redo command */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-console */
+/* eslint-disable no-plusplus */
 const Eris = require('eris');
 const fetch = require('node-fetch');
 const sqlite3 = require('sqlite3');
@@ -44,11 +43,14 @@ client.registerCommand('notifyme', async (message, args) => {
       }
     });
     if (c === true) {
-      console.log(c);
       const search = searchList.data.Page.media[0];
       await db.all('SELECT * FROM watching WHERE malID = (?)', [search.idMal], (err, row) => {
         if (err || row.length === 0) {
           db.run('INSERT INTO watching (userID, malID, aniID, nextEP, title, notified) values (?,?,?,?,?,?)', [message.member.id, search.idMal, search.id, search.nextAiringEpisode.airingAt, search.title.romaji, 0]);
+          const d = new Date(search.nextAiringEpisode.airingAt * 1000);
+          console.log('notify');
+          console.log(message.member.id + search.title);
+          message.channel.createMessage(`You will now be notified for ${search.title.romaji}. Next ep at ${d}\nMAL: ${malLink}${search.idMal}\nanilist: ${aniLink}${search.id}`);
         }
       });
     }
@@ -76,11 +78,15 @@ client.registerCommand('unnotifyme', async (message, args) => {
     if (c === true) {
       const search = searchList.data.Page.media[0];
       db.run('DELETE FROM watching WHERE malID = (?) AND userID = (?)', [search.idMal, message.member.id]);
+      console.log('unnotify');
+      console.log(message.member.id + search.title);
+      client.createMessage(message.channel.id, `You will no longer get notifications for ${searchList.data.Page.media[0].title.romaji} mal: ${search.idMal}`);
     }
   }
 });
 
 function bigBrother() {
+  console.log('Im always watching');
   setInterval(async () => {
     await db.all('select * FROM watching', (err, row) => {
       row.forEach(async (e) => {
@@ -89,6 +95,7 @@ function bigBrother() {
           // console.log(e)
           await notificationSender(e);
           db.run('UPDATE watching SET notified = (?) WHERE malID = (?) AND userID = (?)', [1, e.malID, e.userID]);
+          console.log(`setting notified for ${e.userID} on ${e.title}(${e.malID})`);
         }
       });
     });
@@ -96,6 +103,7 @@ function bigBrother() {
 }
 checkUpdate();
 async function checkUpdate() {
+  console.log('checking for updated times');
   setInterval(() => {
     db.all('SELECT * FROM watching WHERE notified = (?)', [1], (err, row) => {
       if (row.length !== 0) {
@@ -109,6 +117,7 @@ async function checkUpdate() {
             };
             const res = await apiCall(vars);
             if (e.nextEP !== res.nextAiringEpisode.airingAt && e.nextEP < res.nextAiringEpisode.airingAt) {
+              console.log(`Updating ${e.malID} airtime`);
               db.run('UPDATE watching SET notified = (?), nextEP = (?) WHERE malID = (?)', [0, res.nextAiringEpisode.airingAt, e.malID]);
             }
           }, 2500 * index);
@@ -122,6 +131,7 @@ async function notificationSender(row) {
   // console.log(row)
   // console.log(row.userID)
   const chat = await client.getDMChannel(row.userID);
+  console.log(`sending notification for ${row.title}(${row.malID}) to ${row.userID}`);
   client.createMessage(chat.id, `${row.title} is now airing. 
   Mal: ${malLink + row.malID}
   Anilist: ${aniLink + row.aniID}`);
@@ -140,6 +150,7 @@ async function apiCall(vars) {
     }),
   };
 
+  console.log('calling API');
   const searchList = await fetch(apiURL, options)
     .then(res => res.json());
   return searchList;
@@ -177,3 +188,56 @@ const query = `query ($status: MediaStatus, $idMal: Int, $id: Int, $page: Int, $
     }
   }
 }`;
+
+client.registerCommand('search', async (message, args) => {
+  const search = [];
+
+  let vars = {
+    page: 1,
+  };
+
+  let typeLoc = -1;
+  if (args.indexOf('-t') !== -1) {
+    typeLoc = args.indexOf('-t') + 1;
+    vars = Object.assign({ type: args[typeLoc].toUpperCase() }, vars);
+  }
+
+  let pn = 3;
+  let pnLoc = -1;
+  if (args.indexOf('-n') !== -1) {
+    pnLoc = args.indexOf('-n') + 1;
+    if (args[pnLoc] < 10) {
+      pn = Number(args[pnLoc]);
+    }
+    if (args[pnLoc] > 10) {
+      pn = 3;
+    }
+  }
+  vars = Object.assign({ perPage: pn }, vars);
+
+  args.forEach((i, index) => {
+    if (index > typeLoc && index > pnLoc) {
+      search.push(i);
+    }
+  });
+  const txtSearch = search.join(' ');
+  vars = Object.assign({ search: txtSearch }, vars);
+
+  // console.log(vars);
+  const searchList = await apiCall(vars);
+  const search2 = searchList.data.Page.media;
+  const msg = [];
+  for (let i = 0; i < search2.length; i++) {
+    let eTitle;
+    if (search2[i].title.english == null) {
+      eTitle = search2[i].title.romaji;
+    }
+    if (search2[i].title.english != null) {
+      eTitle = search2[i].title.english;
+    }
+    msg.push(`title: ${search2[i].title.romaji} (${eTitle})\ntype: ${search2[i].type}\nlink: https://myanimelist.net/${search2[i].type.toLowerCase()}/${search2[i].idMal} `);
+  }
+  const res = msg.join('\n\n');
+  // console.log(res);
+  message.channel.createMessage(res);
+});
